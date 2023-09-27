7e75efdca707eab816c8997934a1a440504e9b95
Khoa Phan <pldkhoa96.work@gmail.com>
2023-09-27 21:23:49 +0700

    add grid layout effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,6 +47,14 @@ module.exports = {
                 "davy-gray": "#575A5E",
                 "eerie-black": "#222823",
             },
+            gridTemplateRows: {
+                7: "repeat(7, minmax(0, 1fr))",
+                8: "repeat(8, minmax(0, 1fr))",
+                9: "repeat(9, minmax(0, 1fr))",
+                10: "repeat(10, minmax(0, 1fr))",
+                11: "repeat(11, minmax(0, 1fr))",
+                12: "repeat(12, minmax(0, 1fr))",
+            },
             fontFamily: {
                 montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
                 titillium: ["Titillium Web", ...defaultTheme.fontFamily.sans],
